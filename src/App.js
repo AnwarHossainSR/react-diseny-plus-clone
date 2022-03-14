dceb149982e60019468252df67013743f69314b8
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-14 19:17:03 +0600

    Design header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 import Login from "./components/Login";
 const App = () => {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
