85f50f3f245b82263391cc5404da7fbd2b519204
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-14 13:34:54 +0600

    install dependencies and setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
